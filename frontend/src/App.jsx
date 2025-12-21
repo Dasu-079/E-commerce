ba36f4a47b5cf14b453d326e99b4f7f983898af1
Dasu-079 <karridasu079@gmail.com>
2025-12-21 15:28:51 +0530

    Routers applied

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import LoginSignUp from './Pages/LoginSignUp'
+import Shop from './Pages/Shop' 
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory category="mens" />} />
+        <Route path="/womens" element={<ShopCategory category="womens" />} />
+        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignUp />} />
+      </Routes>
+    </>
   )
 }
 
